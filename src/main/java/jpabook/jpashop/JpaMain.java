bf4493f18c804965edd51db8e6febc7b4f841472
Yerim Hyun <gdf4013@naver.com>
2022-04-19 13:25:44 +0900

    JPQL

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -3,10 +3,7 @@ package jpabook.jpashop;
 import jpabook.jpashop.domain.*;
 import net.bytebuddy.asm.Advice;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,7 +19,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
+            /*Member member = new Member();
             member.setUsername("member1");
             member.setHomeAddress(new Address("homeCity1", "street1", "1"));
 
@@ -41,14 +38,25 @@ public class JpaMain {
             System.out.println("==========start==========");
             Member findMember = em.find(Member.class, member.getId());
 
-            Address oldAddress = findMember.getHomeAddress();
+            Address oldAddress = findMember.getHomeAddress();*/
             //findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
 
             //findMember.getFavoriteFood().remove("1");
             //findMember.getFavoriteFood().add("food");
 
-            findMember.getAddressHistory().remove(new Address("old1", "oldstreet1", "old1"));
-            findMember.getAddressHistory().add(new Address("newCity2", "oldstreet1", "old1"));
+//            findMember.getAddressHistory().remove(new Address("old1", "oldstreet1", "old1"));
+//            findMember.getAddressHistory().add(new Address("newCity2", "oldstreet1", "old1"));
+            Member member = new Member();
+            member.setUsername("yerim");
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            em.createQuery("select new jpql.MemberDTO(m.username, m.id) from Member as m", MemberDTO.class);
 
             tx.commit();
         } catch (Exception e) {
